d4dbe71920d242f7f23d7888aa6d252e06bf2040
Sourav@2820 <souravmukherjee1111999@gmail.com>
2024-07-11 14:12:12 +0530

    practice qs updation

--- practiceQS/prac2.js
+++ practiceQS/prac2.js
@@ -1,4 +1,4 @@
 let h2 = document.querySelector("h2")
 console.dir(h2.innerText);
 
-h2.innerText = h2.innerText + " From Apna Collage Students.";
+h2.innerText = h2.innerText + " From Apna Collage Students."; //Concatination
---
+++ practiceQS/prac3.js
@@ -0,0 +1,7 @@
+// let div = document.querySelector("div");
+// console.log(div);
+// let id = div.getAttribute("id");
+// console.log("id")
+
+let div = document.querySelector("div");//Creating object of div
+div.style.backgroundColor = "blue";//Change the propertics of div by style attributes.
