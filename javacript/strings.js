0f4e3adab8b80d7f67095a4c6994a9051eacb72e
Sourav@2820 <souravmukherjee1111999@gmail.com>
2024-07-13 21:49:57 +0530

    updation

---
+++ javacript/strings.js
@@ -0,0 +1,11 @@
+let str = "Sourav";
+let str2 = "Mukherjee"
+//String are immutable
+console.log(str.toUpperCase()); // For convert whole string in Upper case
+console.log(str.toLowerCase()); // For convert whole string in Lower case
+console.log(str.trim());// For remove White spaces
+console.log(str.slice(0,3));// For print selected text
+console.log(str.concat(str2));//For add two strings
+console.log(str.replace("o","k"));// For replace index value/Character
+console.log(str.charAt(2));//For print index value
+
